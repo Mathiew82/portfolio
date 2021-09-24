b742d05f9b6360468b38f8d9624695b8828341da
Mathiew82 <amateo1982@yahoo.es>
2021-09-24 19:05:41 +0200

    Agregada la nueva foto de perfil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <Welcome
-    image="mi-foto-min.png"
+    image="mi-foto-min.jpeg"
     title="BIENVENID@ A MI PORTFOLIO!!"
     description="Soy Alberto Mateo y me dedico al desarrollo de aplicaciones como Frontend"
   />
