ccca58c1a4390754f14b702b9590cebccea9fc3e
Mathiew82 <amateo1982@yahoo.es>
2022-04-10 02:40:48 +0200

    Agregado el proyecto dailup en proyectos

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,5 +1,29 @@
 export const projectsData = [
   {
+    image: 'logo_dailup-min.jpeg',
+    url: 'https://dailup.es/',
+    name: 'Dailup',
+    stack: 'Vue 3, Pinia, SASS',
+    description:
+      'Sencilla aplicación para controlar el tiempo de las dailys.',
+  },
+  {
+    image: 'logo_cijs-min.png',
+    url: 'https://compress-images-js.com/',
+    name: 'Compress Images JS',
+    stack: 'Vue 2, Vuex, SASS',
+    description:
+      'Aplicación para la compresión de imágenes. Una vez comprimidas, pueden descargarse individualmente o en un archivo comprimido.',
+  },
+  {
+    image: 'logo_buscamovies-min.png',
+    url: 'http://buscamovies.surge.sh/',
+    name: 'BuscaMovies',
+    stack: 'React, Redux, Jest, TMDb API',
+    description:
+      'Proyecto de búsqueda de películas alimentado por la api de TMDb. Puedes buscar películas y obtener la información sobre cualquiera de ellas.',
+  },
+  {
     image: 'logo_multiplication-tables-desktop-min.png',
     url: 'https://play.google.com/store/apps/details?id=com.multiplicationtablesmobileapp',
     name: 'Tablas de Multiplicar - Mobile App',
@@ -14,21 +38,5 @@ export const projectsData = [
     stack: 'JavaScript, Electron, SASS',
     description:
       'Un sencillo juego con el que los niños podrán aprender de una forma más divertida y práctica las tablas de multiplicar.',
-  },
-  {
-    image: 'logo_buscamovies-min.png',
-    url: 'http://buscamovies.surge.sh/',
-    name: 'BuscaMovies',
-    stack: 'React, Redux, Jest, TMDb API',
-    description:
-      'Proyecto de búsqueda de películas alimentado por la api de TMDb. Puedes buscar películas y obtener la información sobre cualquiera de ellas.',
-  },
-  {
-    image: 'logo_cijs-min.png',
-    url: 'https://compress-images-js.com/',
-    name: 'Compress Images JS',
-    stack: 'Vue.js 2, Vuex, SASS',
-    description:
-      'Aplicación para la compresión de imágenes. Puede comprimir imágenes de cualquier formato generando procesos consecutivos por cada una de las imágenes. Una vez comprimidas, pueden descargarse individualmente o en un archivo comprimido.',
   },
 ]
