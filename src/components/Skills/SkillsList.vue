6c9239332f889361483d18a2c654e0c10d5d70da
Mathiew82 <amateo1982@yahoo.es>
2023-04-19 01:26:32 +0200

    Agregado el tipo Skill a la constante skills del componente SkillList

--- src/components/Skills/SkillsList.vue
+++ src/components/Skills/SkillsList.vue
@@ -2,19 +2,20 @@
   <ul class="skills">
     <SkillItem
       v-for="skill in skills"
-      :key="skill"
+      :key="skill.name"
       :color="skill.color"
       :name="skill.name"
     />
   </ul>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onBeforeMount } from 'vue';
-import { skillsData } from '@/constants/skills.ts';
+import { skillsData } from '@/constants/skills';
+import { Skill } from '@/types/Skill';
 import SkillItem from './SkillItem.vue';
 
-const skills = ref(null);
+const skills = ref<Skill[]>(null);
 
 onBeforeMount(() => {
   skills.value = skillsData;
