e8d203a60182c5bb53bbaf054e8a0d3e0879aa8c
Mathiew82 <amateo1982@yahoo.es>
2023-04-19 00:25:51 +0200

    Modificadas las descripciones en las experiencias

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -4,35 +4,35 @@ export const experienceData = [
     companyName: 'Promofarma',
     position: 'Frontend Engineer',
     dates: 'jun. 2021 - actualidad',
-    description: `Tecnologías: HTML5, CSS3, SASS, JavaScript, Typescript, Vue.js, Vuex, GraphQL, Jest, GIT.`,
+    description: `Tecnologías: HTML, CSS, SASS, JavaScript, Typescript, Vue.js, Vuex, GraphQL, Jest, GIT`
   },
   {
     image: 'logo_housfy-min.jpg',
     companyName: 'Housfy',
     position: 'Front-End Developer',
     dates: 'dic. 2020 - abr. 2021',
-    description: `Tecnologías: HTML5, CSS3, SASS, JavaScript, Vue.js, Nuxt.js, Vuex, Jest, GIT.`,
+    description: `Tecnologías: HTML, CSS, SASS, JavaScript, Vue.js, Nuxt.js, Vuex, Jest, GIT`
   },
   {
     image: 'logo_crealogix-min.png',
     companyName: 'CREALOGIX Group',
     position: 'Front-End Developer',
     dates: 'jul. 2020 - dic. 2020',
-    description: `Tecnologías: HTML5, CSS3, SASS, JavaScript, GIT.`,
+    description: `Tecnologías: HTML, CSS, SASS, JavaScript, GIT`
   },
   {
     image: 'logo_ohmybrand-min.png',
     companyName: 'Oh! my brand',
     position: 'Front-End Developer',
     dates: 'oct. 2018 - jul. 2020',
-    description: `Tecnologías: HTML5, CSS3, SASS, JavaScript, Vue.js, Vuex, Vue StoreFront, Riot.js, GIT.`,
+    description: `Tecnologías: HTML, CSS, SASS, JavaScript, Vue.js, Vuex, Vue StoreFront, Riot.js, GIT`
   },
   {
     image: 'logo_pasiona-min.jpg',
     companyName: 'Pasiona',
     position: 'JavaScript Developer',
     dates: 'nov. 2017 - jun. 2018',
-    description: `Tecnologías: JavaScript, Phaser, Karma, Scrum, GIT.`,
+    description: `Tecnologías: JavaScript, Phaser, Karma, Scrum, GIT`
   },
   {
     image: 'logo_proogresa-min.png',
@@ -40,13 +40,13 @@ export const experienceData = [
     position: 'Full-Stack Developer',
     dates: 'oct. 2016 - nov. 2017',
     description:
-      'Tecnologías: HTML5, CSS3, SASS, JavaScript, jQuery, AJAX, PHP, Twig, CodeIgniter, MySQL, GIT.',
+      'Tecnologías: HTML, CSS, SASS, JavaScript, jQuery, AJAX, PHP, Twig, CodeIgniter, MySQL, GIT'
   },
   {
     image: 'logo_codiwho-min.png',
     companyName: 'Freelance',
     position: 'Full-Stack Developer',
     dates: 'dic. 2013 - mar. 2015',
-    description: `Tecnologías: HTML5, CSS3, SASS, JavaScript, jQuery, AJAX, PHP, CodeIgniter, MySQL, GIT.`,
-  },
-]
+    description: `Tecnologías: HTML, CSS, SASS, JavaScript, jQuery, AJAX, PHP, CodeIgniter, MySQL, GIT`
+  }
+];
