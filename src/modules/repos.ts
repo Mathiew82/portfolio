9dd5c27615d0464fc2613a168d63556d602751a8
Mathiew82 <amateo1982@yahoo.es>
2025-12-29 19:39:50 +0100

    feat(a11y): improve accessibility of external links

--- src/modules/repos.ts
+++ src/modules/repos.ts
@@ -43,7 +43,7 @@ const displayRepos = (): void => {
     const repoElement = document.createElement("div");
     repoElement.className = "col xs-12 sm-6 md-4 lg-4";
     repoElement.innerHTML = `<div class="repos__item">
-          <a href="${item.html_url}" target="_blank" class="repos-title">
+          <a href="${item.html_url}" target="_blank" rel="noopener noreferrer" aria-label="View repository (opens in a new tab)" class="repos-title">
             <svg
               height="512pt"
               viewBox="0 -57 512 512"
