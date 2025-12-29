f290ec91f7d91e9cbcebcb22c23f1343a8bd36b7
Mathiew82 <amateo1982@yahoo.es>
2025-12-30 00:48:43 +0100

    feat: change the order of the projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,22 +2,6 @@ import type { ProjectItem } from "../types/ProjectItem";
 
 export const projects: ProjectItem[] = [
   {
-    image: "logo_curripulum-min.webp",
-    url: "https://curripulum.vercel.app/",
-    name: "Curripulum",
-    stack: "React",
-    description:
-      "Application to easily and practically generate a printable resume with basic information.",
-  },
-  {
-    image: "logo_dailup-min.webp",
-    url: "https://dailup.vercel.app/",
-    name: "Dailup",
-    stack: "Vue 3, Pinia",
-    description:
-      "Application for tracking daily work time. Tracks time by shift.",
-  },
-  {
     image: "logo_cijs-min.webp",
     url: "https://compress-images-js.vercel.app/",
     name: "Compress Images JS",
@@ -26,12 +10,28 @@ export const projects: ProjectItem[] = [
       "Image compression application. Images can be downloaded individually or in a compressed file.",
   },
   {
+    image: "logo_curripulum-min.webp",
+    url: "https://curripulum.vercel.app/",
+    name: "Curripulum",
+    stack: "React",
+    description:
+      "Application to easily and practically generate a printable resume with basic information.",
+  },
+  {
     image: "logo_buscamovies-min.webp",
     url: "https://buscamovies.vercel.app/",
     name: "BuscaMovies",
     stack: "React, Redux, Jest, TMDb API",
     description:
       "Movie search project powered by the TMDb API. You can search for movies and get information about them.",
+  },
+  {
+    image: "logo_dailup-min.webp",
+    url: "https://dailup.vercel.app/",
+    name: "Dailup",
+    stack: "Vue 3, Pinia",
+    description:
+      "Application for tracking daily work time. Tracks time by shift.",
   },
   {
     image: "logo_multiplication-tables-desktop-min.webp",
